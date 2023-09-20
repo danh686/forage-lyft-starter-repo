20a53ca6ae1d452ae47d73baa11a9caf23fb1348
Daniel Harris <dan.harris686@gmail.com>
2023-09-20 15:02:06 +0100

    Fix bug

--- models/thovex.py
+++ models/thovex.py
@@ -6,7 +6,6 @@ from batteries.nubbin_battery import NubbinBattery
 class Thovex(Car):
     def __init__(self, current_mileage, last_service_mileage, last_service_date):
         super().__init__(
-            self,
             last_service_date,
             CapuletEngine(current_mileage, last_service_mileage),
             NubbinBattery(last_service_date),
