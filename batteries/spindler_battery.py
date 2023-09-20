8d90640ab877f444b658fd5ca60433288f2f6c21
Daniel Harris <dan.harris686@gmail.com>
2023-09-20 12:38:03 +0100

    Add batteries

---
+++ batteries/spindler_battery.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from battery import Battery
+
+
+class SpindlerBattery(Battery):
+    def __init__(self, last_service_date):
+        super().__init__(last_service_date)
+
+    def needs_service(self):
+        service_threshold_date = self.last_service_date.replace(
+            year=self.last_service_date.year + 3
+        )
+
+        if service_threshold_date < datetime.today().date():
+            return True
+        else:
+            return False
